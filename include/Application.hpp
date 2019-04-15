1f9fe72ed33dedf350f7de1e9d80cb314a569e13
sarahkittyy <chrisohlin1@gmail.com>
2019-04-15 15:12:40 -0400

    initial commit

---
+++ include/Application.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include <imgui/imgui-SFML.h>
+#include <imgui/imgui.h>
+
+/**
+ * @brief Base app class.
+ * 
+ */
+class Application
+{
+public:
+	/**
+	 * @brief Init the app.
+	 * 
+	 */
+	Application();
+
+	/**
+	 * @brief Main app loop, equivalent to main().
+	 * 
+	 * @return int Exit code of the program.
+	 */
+	int run();
+
+private:
+	/**
+	 * @brief The app window.
+	 * 
+	 */
+	sf::RenderWindow mWindow;
+};
---
+++ src/Application.cpp
@@ -0,0 +1,59 @@
+#include "Application.hpp"
+
+Application::Application()
+	: mWindow(sf::VideoMode(700, 700), "Wireworld")
+{
+	//Initialize ImGui..
+	ImGui::SFML::Init(mWindow);
+}
+
+int Application::run()
+{
+	//The ImGui internal clock.
+	sf::Clock imgui_clock;
+
+	//App loop.
+	while (mWindow.isOpen())
+	{
+		//Handle events.
+		sf::Event event;
+		while (mWindow.pollEvent(event))
+		{
+			//Process ImGui events.
+			ImGui::SFML::ProcessEvent(event);
+
+			//SFML events..
+			switch (event.type)
+			{
+			default:
+				break;
+			case sf::Event::Closed:
+				mWindow.close();
+				break;
+			case sf::Event::Resized:
+				mWindow.setSize({event.size.width, event.size.height});
+				break;
+			}
+		}
+		//Update ImGui...
+		ImGui::SFML::Update(mWindow, imgui_clock.restart());
+		//Draw to imgui...
+		ImGui::Begin("h");
+		ImGui::End();
+
+		//Stop drawing to imgui..
+		ImGui::EndFrame();
+
+		//Clear the window.
+		mWindow.clear(sf::Color::White);
+		//Draw here...
+
+
+		//Render ImGui
+		ImGui::SFML::Render(mWindow);
+		//Finish drawing.
+		mWindow.display();
+	}
+
+	return 0;
+}
